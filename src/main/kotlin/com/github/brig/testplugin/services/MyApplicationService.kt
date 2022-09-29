b33f40f829f9f9acd2853e1df42b3b396f414b03
GitHub Action <action@github.com>
2022-09-29 08:19:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/brig/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/brig/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.brig.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.brig.testplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/brig/testplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/brig/testplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.brig.testplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.brig.testplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
