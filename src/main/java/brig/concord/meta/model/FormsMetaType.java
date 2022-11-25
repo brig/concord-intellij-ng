054c32f84f590ddf3c771a9ed2bdd790d8aa9cdf
Yury Brigadirenko <ybrigo@gmail.com>
2022-11-25 15:49:14 +0300

    forms default autocomplete

--- src/main/java/brig/concord/meta/model/FormsMetaType.java
+++ src/main/java/brig/concord/meta/model/FormsMetaType.java
@@ -1,11 +1,19 @@
 package brig.concord.meta.model;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.yaml.meta.model.Field;
 import org.jetbrains.yaml.meta.model.YamlArrayType;
 import org.jetbrains.yaml.meta.model.YamlMetaType;
+import org.jetbrains.yaml.psi.YAMLMapping;
+
+import java.util.List;
 
 public class FormsMetaType extends MapMetaType {
 
     private static final FormsMetaType INSTANCE = new FormsMetaType();
+
+    private static final List<Field> defaultCompletions = List.of(new Field("myForm", FormFieldsMetaType.getInstance()));
 
     protected FormsMetaType() {
         super("Form definition");
@@ -20,9 +28,16 @@ public class FormsMetaType extends MapMetaType {
         return FormFieldsMetaType.getInstance();
     }
 
+    @Override
+    public @NotNull List<Field> computeKeyCompletions(@Nullable YAMLMapping existingMapping) {
+        return defaultCompletions;
+    }
+
     private static class FieldsWrapper extends MapMetaType {
 
         private static final FieldsWrapper INSTANCE = new FieldsWrapper();
+
+        private static final List<Field> defaultCompletions = List.of(new Field("myField", FormFieldMetaType.getInstance()));
 
         public static FieldsWrapper getInstance() {
             return INSTANCE;
@@ -35,6 +50,11 @@ public class FormsMetaType extends MapMetaType {
         @Override
         protected YamlMetaType getMapEntryType(String name) {
             return FormFieldMetaType.getInstance();
+        }
+
+        @Override
+        public @NotNull List<Field> computeKeyCompletions(@Nullable YAMLMapping existingMapping) {
+            return defaultCompletions;
         }
     }
 
