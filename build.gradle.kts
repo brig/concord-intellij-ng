341fd8870ac95bdae9bd74b5436c18689d937ef9
brig <ybrigo@gmail.com>
2024-08-08 21:56:22 -0400

    qodana version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.13"
+    id("org.jetbrains.qodana") version "2024.1.9"
 }
 
 group = properties("pluginGroup")
