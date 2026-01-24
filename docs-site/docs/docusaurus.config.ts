d80fa239f5ba27a5b8429e6235e4c49f4225943c
brig <ybrigo@gmail.com>
2026-01-24 15:17:16 -0500

    docs init

---
+++ docs-site/docs/docusaurus.config.ts
@@ -0,0 +1,26 @@
+const config = {
+    url: 'https://brig.github.io',
+    baseUrl: '/concord-intellij-ng/',
+    organizationName: 'brig',
+    projectName: 'concord-intellij-ng',
+    trailingSlash: false,
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.ts'),
+                    routeBasePath: 'docs',
+                    editUrl: 'https://github.com/brig/concord-intellij-ng/tree/main/docs-site/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    editUrl: 'https://github.com/brig/concord-intellij-ng/tree/main/docs-site/',
+                },
+                theme: { customCss: require.resolve('./src/css/custom.css') },
+            },
+        ],
+    ],
+};
+export default config;
