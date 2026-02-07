3729c0bf47748e3e1a0b35d437acd7197f011eb5
brig <ybrigo@gmail.com>
2026-02-07 03:57:43 -0500

    project: intellij plugins version up

--- perf-tester/build.gradle.kts
+++ perf-tester/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.intellij.platform") version "2.11.0"
-    id("org.jetbrains.kotlin.jvm") version "2.2.0"
+    id("org.jetbrains.kotlin.jvm") version "2.3.10"
 }
 
 repositories {
