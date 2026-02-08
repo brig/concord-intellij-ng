d6e37e1416cd6c45d37932d1b35d18ca424afd5e
brig <ybrigo@gmail.com>
2026-02-07 23:21:28 -0500

    call in params cleanup

--- src/main/java/brig/concord/meta/model/ParamMetaTypes.java
+++ src/main/java/brig/concord/meta/model/ParamMetaTypes.java
@@ -1,19 +1,19 @@
 package brig.concord.meta.model;
 
-import brig.concord.meta.model.call.*;
+import brig.concord.meta.model.call.AnyMapInParamMetaType;
 
 public final class ParamMetaTypes {
 
     private ParamMetaTypes() {}
 
-    public static final AnyOfType STRING_OR_EXPRESSION = AnyOfType.anyOf(StringInParamMetaType.getInstance(), ExpressionMetaType.getInstance());
-    public static final AnyOfType BOOLEAN_OR_EXPRESSION = AnyOfType.anyOf(BooleanInParamMetaType.getInstance(), ExpressionMetaType.getInstance());
-    public static final AnyOfType NUMBER_OR_EXPRESSION = AnyOfType.anyOf(IntegerInParamMetaType.getInstance(), ExpressionMetaType.getInstance());
+    public static final AnyOfType STRING_OR_EXPRESSION = AnyOfType.anyOf(StringMetaType.getInstance(), ExpressionMetaType.getInstance());
+    public static final AnyOfType BOOLEAN_OR_EXPRESSION = AnyOfType.anyOf(BooleanMetaType.getInstance(), ExpressionMetaType.getInstance());
+    public static final AnyOfType NUMBER_OR_EXPRESSION = AnyOfType.anyOf(IntegerMetaType.getInstance(), ExpressionMetaType.getInstance());
     public static final AnyOfType OBJECT_OR_EXPRESSION = AnyOfType.anyOf(AnyMapInParamMetaType.getInstance(), ExpressionMetaType.getInstance());
 
-    public static final AnyOfType STRING_ARRAY_OR_EXPRESSION = AnyOfType.anyOf(StringArrayInParamMetaType.getInstance(), ExpressionMetaType.getInstance());
-    public static final AnyOfType BOOLEAN_ARRAY_OR_EXPRESSION = AnyOfType.anyOf(BooleanArrayInParamMetaType.getInstance(), ExpressionMetaType.getInstance());
-    public static final AnyOfType OBJECT_ARRAY_OR_EXPRESSION = AnyOfType.anyOf(ObjectArrayInParamMetaType.getInstance(), ExpressionMetaType.getInstance());
-    public static final AnyOfType NUMBER_ARRAY_OR_EXPRESSION = AnyOfType.anyOf(IntegerArrayInParamMetaType.getInstance(), ExpressionMetaType.getInstance());
-    public static final AnyOfType ARRAY_OR_EXPRESSION = AnyOfType.anyOf(AnyArrayInParamMetaType.getInstance(), ExpressionMetaType.getInstance());
+    public static final AnyOfType STRING_ARRAY_OR_EXPRESSION = AnyOfType.anyOf(StringArrayMetaType.getInstance(), ExpressionMetaType.getInstance());
+    public static final AnyOfType BOOLEAN_ARRAY_OR_EXPRESSION = AnyOfType.anyOf(BooleanArrayMetaType.getInstance(), ExpressionMetaType.getInstance());
+    public static final AnyOfType OBJECT_ARRAY_OR_EXPRESSION = AnyOfType.anyOf(ObjectArrayMetaType.getInstance(), ExpressionMetaType.getInstance());
+    public static final AnyOfType NUMBER_ARRAY_OR_EXPRESSION = AnyOfType.anyOf(IntegerArrayMetaType.getInstance(), ExpressionMetaType.getInstance());
+    public static final AnyOfType ARRAY_OR_EXPRESSION = AnyOfType.anyOf(AnyArrayMetaType.getInstance(), ExpressionMetaType.getInstance());
 }
--- src/main/java/brig/concord/meta/model/call/AnyMapInParamMetaType.java
+++ src/main/java/brig/concord/meta/model/call/AnyMapInParamMetaType.java
@@ -1,10 +1,6 @@
 package brig.concord.meta.model.call;
 
 import brig.concord.meta.model.AnyMapMetaType;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import brig.concord.yaml.meta.model.Field;
-import brig.concord.yaml.meta.model.YamlMetaType;
 
 public class AnyMapInParamMetaType extends AnyMapMetaType implements CallInParamMetaType {
 
@@ -12,15 +8,5 @@ public class AnyMapInParamMetaType extends AnyMapMetaType implements CallInParam
 
     public static AnyMapInParamMetaType getInstance() {
         return INSTANCE;
-    }
-
-    @Override
-    public @Nullable Field findFeatureByName(@NotNull String name) {
-        return AnyInParamMetaType.getInstance().findFeatureByName(name);
-    }
-
-    @Override
-    protected YamlMetaType getMapEntryType(String name) {
-        return AnyInParamMetaType.getInstance();
     }
 }
